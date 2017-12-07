39712c80bd03ffb5042507b8ab0c295fdcf2f188
maksym.zelenin <maksym.zelenin@Expedition.edvantis.com>
2017-12-07 13:39:32 +0200

    UserEngine param name was chahged

--- src/main/java/com/musicinstruments/engine/UserEngine.java
+++ src/main/java/com/musicinstruments/engine/UserEngine.java
@@ -5,9 +5,9 @@ import com.musicinstruments.entity.User;
 public interface UserEngine {
 
 	public User findUserByEmail(String email);	
-	public User register(String name, String hashedPassword, String address, String phone, String mail);
+	public User register(String name, String hashedPassword, String address, String phone, String email);
 	public void updateProfile();
-	public void logIn(String mail, String password);
+	public void logIn(String email, String password);
 	public void logOut();
 	public void createOrder();
 	
