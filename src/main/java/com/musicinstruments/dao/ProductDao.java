dea0216bee0aba69e7166018a3c8bb1331416136
maksym.zelenin <maksym.zelenin@Expedition.edvantis.com>
2017-12-07 19:08:10 +0200

    Abstract Dao class was created

--- src/main/java/com/musicinstruments/dao/ProductDao.java
+++ src/main/java/com/musicinstruments/dao/ProductDao.java
@@ -1,5 +1,12 @@
 package com.musicinstruments.dao;
 
+import org.hibernate.SessionFactory;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
 public class ProductDao {
 
+	private SessionFactory sessionFactory;
+	
+	public Product getProductByID()
 }
